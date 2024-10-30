aede8e56ffe289443b8570b44b79fa4eaf5424f4
Polina <polipet02@gmail.com>
2024-10-29 23:33:48 -0400

    adding data import

--- README.md
+++ README.md
@@ -11,5 +11,20 @@ We used the open-source Galaxy Zoo 2 dataset, which contains classified galaxy i
 
 ### Repository Structure
 Due to the large volume of data used in this project, we were unable to include it in the repository. Below is a map of the file structure. 
+```
+Parent directory on local machine
+├── 📁 CS682-Project
+│   ├── 📁 classifiers
+│   │   └── CNN.py
+│   │   └── data_processing.py
+├── 📁 data
+│   │   └── gz2_hart16.csv
+│   │   └── gz2_filename_mapping.csv
+│   ├── 📁 images_gz2
+│   │   ├── 📁 images
+│   │   ├── results.txt
+│   │   └── results_public.txt
+│   │   └── *.jpg
+```
 
 ### Packages
--- classifiers/data_preprocessing.py
+++ classifiers/data_preprocessing.py
@@ -1,24 +1,26 @@
 # imports
 import sqlite3
 from sklearn.model_selection import train_test_split
-import gdown
-
-# Replace with the shared link or file ID
-url = 'https://drive.google.com/uc?id=your_file_id'
-output = 'file_name.extension'
-gdown.download(url, output, quiet=False)
 
 
+# # downloaded hart16 files from https://data.galaxyzoo.org/#section-7
+# # downloaded images and mapping from https://www.kaggle.com/datasets/jaimetrickz/galaxy-zoo-2-images/data
+# FILENAME_MAPPINGS = "data/gz2_filename_mapping.csv"
+# HART16 = "data/gz2_hart16.csv"
 
-# download hart16 files from: https://data.galaxyzoo.org/#section-12
-# download images and mapping from: https://zenodo.org/record/3565489#.Y3vFKS-l0eY
-FILENAME_MAPPINGS = "data/gz2_filename_mapping.csv"
-HART16 = "data/gz2_hart16.csv"
-
-ORIGINAL_IMAGES_DIR = "data/images/"
-PROCESSED_IMAGES_DIR = "data/images_processed/"
-RANDOM_STATE = 32
-TRAIN_IMAGES_DIR = "data/train_images/"
-TEST_IMAGES_DIR = "data/test_images/"
+# ORIGINAL_IMAGES_DIR = "data/images/"
+# PROCESSED_IMAGES_DIR = "data/images_processed/"
+# RANDOM_STATE = 32
+# TRAIN_IMAGES_DIR = "data/train_images/"
+# TEST_IMAGES_DIR = "data/test_images/"
 
 
+import pandas as pd
+
+# follow the directory structure specified in the repository 
+# downloaded heart16 file from https://data.galaxyzoo.org/#section-7
+df_hart16 = pd.read_csv('../data/gz2_hart16.csv')
+df_hart16.info()
+# downloaded images and mapping from https://www.kaggle.com/datasets/jaimetrickz/galaxy-zoo-2-images/data
+df_mappings = pd.read_csv('../data/gz2_filename_mapping.csv')
+df_mappings.info()
