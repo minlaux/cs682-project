eced8dfb5339fcb0ebf893c28b0ac133b6116e7e
Polina <polipet02@gmail.com>
2024-11-29 20:11:45 -0500

    processed images through google colab

--- classifiers/CNN.py
+++ classifiers/CNN.py
@@ -1,15 +1,3 @@
-# import torch
-# import torch.nn as nn
-# import torch.optim as optim
-# from torch.utils.data import DataLoader
-# from torch.utils.data import sampler
-# import torchvision.datasets as dset
-# import torchvision.transforms as T
-# import numpy as np
-
-# # set device
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# print('Using device:', device)
 
 # NUM_TRAIN = 49000
 # # The torchvision.transforms package provides tools for preprocessing data
@@ -21,7 +9,7 @@
 #     T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
 #     ])
 
-# # create galaxy map later?
+# create galaxy map later?
 
 import torch
 import torch.nn as nn
